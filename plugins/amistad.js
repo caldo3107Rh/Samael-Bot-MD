f2f709f4a31998062500af1f340f348acbb0c233
Ricardo-Samael <85195129+caldo3107Rh@users.noreply.github.com>
2022-08-11 15:22:41 -0400

    卐🧿𝐒𝚫𝐌𝚫𝚵𝐋-𝚩𝚯𝐓🧿卐

---
+++ plugins/amistad.js
@@ -0,0 +1,13 @@
+let toM = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata }) {
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let b
+do b = ps.getRandom()
+while (b === a)
+m.reply(`*卐🔰 Vamos a hacer algunas amistades 🔰*\n\n*Oye ${toM(a)} hablale al privado a ${toM(b)} para que jueguen y se haga una amistad 🙆*\n\n*Las mejores amistades empiezan con un juego 😉卐*`, null, {
+mentions: [a, b],
+})}
+handler.command = ['amigo','amigorandom','amistad']
+handler.group = true
+export default handler
